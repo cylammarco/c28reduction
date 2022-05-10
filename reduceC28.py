db771aa614dfc91e5c110a178ea379a6a7b55248
cylammarco <cylammarco@gmail.com>
2022-05-10 19:13:43 +0300

    generate list for reduced frame in wcs-fit-only mode

--- reduceC28.py
+++ reduceC28.py
@@ -772,6 +772,17 @@ for folder_i in folder_name:
         or args.build_total_stack_only
     ):
 
+        reduced_light_filename_list = []
+        for filename in filelist_light_raw:
+            # Get the filter
+            _filter = filename.split("-")[2]
+            outfile_name, outfile_extension = os.path.splitext(filename)
+            outfile_name += "-reduced"
+            # Get the file path
+            filepath = os.path.join(folder_i, filename)
+            outfile_filepath = os.path.join(folder_i, outfile_name + outfile_extension)
+            reduced_light_filename_list.append(outfile_filepath)
+
         # Do WCS fit on all of the light frames
         wcs_fit(reduced_light_filename_list)
 
